a29e42335918c730ba3c56a6c6c1075a8300428c
sreeram <sreeram.boyapati2011@gmail.com>
2014-09-23 21:59:30 +0530

    Edited Watershed Algorithm

--- src/segment_images.cpp
+++ src/segment_images.cpp
@@ -37,65 +37,6 @@ Mat NormalizeImage(Mat source){
     return dst;
 }
 
-Mat multi_channelSegmentation(Mat src){
-    Mat binary_image;
-    Mat dist;
-    namedWindow("samples");
-    cvtColor(src, binary_image, CV_BGR2GRAY);
-    //Distance Transform
-    imshow("samples", binary_image);
-    waitKey(0);
-    distanceTransform(binary_image, dist, CV_DIST_L2, 3);
-    //Normalize to [0, 1]
-    imshow("samples", dist);
-    waitKey(0);
-    normalize(dist, dist, 0, 1., NORM_MINMAX);
-    //Take a threshold of 0.2
-    threshold(dist, dist, .2, 1., CV_THRESH_BINARY);
-    //Convert it into 8 Unsigned single channel immage
-    imshow("samples", dist);
-    waitKey(0);
-    Mat dist_8u;
-    dist.convertTo(dist_8u, CV_8U);
-    //Find Markers
-    vector<vector<Point> > contours;
-    findContours(dist_8u, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-    //Total Objects
-    int ncomp = contours.size();
-    //Segmenting foreground Images
-    Mat markers = Mat::zeros(dist.size(), CV_32SC1);
-    imshow("samples", dist);
-    waitKey(0);
-    for (int i = 0; i < ncomp; i++)
-        drawContours(markers, contours, i, cv::Scalar::all(i+1), -1);
-    imshow("samples", dist);
-    waitKey(0);
-    circle(markers, cv::Point(5,5), 3, CV_RGB(255,255,255), -1);
-    watershed(src, markers);
-    cv::Mat dst = cv::Mat::zeros(markers.size(), CV_8UC3);
-    std::vector<cv::Vec3b> colors;
-    for (int i = 0; i < ncomp; i++)
-    {
-        int b = cv::theRNG().uniform(0, 255);
-        int g = cv::theRNG().uniform(0, 255);
-        int r = cv::theRNG().uniform(0, 255);
-
-        colors.push_back(cv::Vec3b((uchar)b, (uchar)g, (uchar)r));
-    }
-    for (int i = 0; i < markers.rows; i++)
-    {
-        for (int j = 0; j < markers.cols; j++)
-        {
-            int index = markers.at<int>(i,j);
-            if (index > 0 && index <= ncomp)
-                dst.at<cv::Vec3b>(i,j) = src.at<cv::Vec3b>(i, j);
-            else
-                dst.at<cv::Vec3b>(i,j) = cv::Vec3b(0,0,0);
-        }
-    }
-    return dst;
-}
-
 Mat SURF_Feature_Detector(Mat src){
     //Hessian Threshold
     int minHession = 400;
@@ -117,6 +58,238 @@ Mat SURF_Feature_Detector(Mat src){
     return descriptors;
 }
 
+
+void mergeSegments(Mat & image,Mat & segments, int & numOfSegments)
+{
+    //To collect pixels from each segment of the image
+    vector<Mat> samples;
+    //In case of multiple merging iterations, the numOfSegments should be updated
+    int newNumOfSegments = numOfSegments;
+
+    //Initialize the segment samples
+    for(int i=0;i<=numOfSegments;i++)
+    {
+        Mat sampleImage;
+        samples.push_back(sampleImage);
+    }
+
+    //collect pixels from each segments
+    for(int i = 0; i < segments.rows; i++ )
+    {
+        for(int j = 0; j < segments.cols; j++ )
+        {
+            //check what segment the image pixel belongs to
+            int index = segments.at<int>(i,j);
+            if(index >= 0 && index<numOfSegments)
+            {
+                samples[index].push_back(image(Rect(j,i,1,1)));
+            }
+        }
+    }
+
+    //create histograms
+    vector<MatND> hist_bases;
+    Mat hsv_base;
+    /// Using 35 bins for hue component
+    int h_bins = 35;
+    /// Using 30 bins for saturation component
+    int s_bins = 30;
+    int histSize[] = { h_bins,s_bins };
+
+    // hue varies from 0 to 256, saturation from 0 to 180
+    float h_ranges[] = { 0, 256 };
+    float s_ranges[] = { 0, 180 };
+
+    const float* ranges[] = { h_ranges, s_ranges };
+
+    // Use the 0-th and 1-st channels
+    int channels[] = { 0,1 };
+
+    // To store the histograms
+    MatND hist_base;
+    for(int c=1;c<numOfSegments;c++)
+    {
+        if(samples[c].dims>0){
+            //convert the sample to HSV
+            cvtColor( samples[c], hsv_base, CV_BGR2HSV );
+            //calculate the histogram
+            calcHist( &hsv_base, 1, channels, Mat(), hist_base,2, histSize, ranges, true, false );
+            //normalize the histogram
+            normalize( hist_base, hist_base, 0, 1, NORM_MINMAX, -1, Mat() );
+            //append to the collection
+            hist_bases.push_back(hist_base);
+        }else
+        {
+            hist_bases.push_back(MatND());
+        }
+
+        hist_base.release();
+    }
+
+    //To store the similarity of histograms
+    double similarity = 0;
+    //to keep the track of already merged segments
+    vector<bool> mearged;
+    //initialize the merged segments tracker
+    for(int k = 0; k < hist_bases.size(); k++)
+    {
+        mearged.push_back(false);
+    }
+
+    //calculate the similarity of the histograms of each pair of segments
+    for(int c=0;c<hist_bases.size();c++)
+    {
+        for(int q=c+1;q<hist_bases.size();q++)
+        {
+            //if the segment is not merged alreay
+            if(!mearged[q])
+            {
+                if(hist_bases[c].dims>0 && hist_bases[q].dims>0)
+                {
+                    //calculate the histogram similarity
+                    similarity = compareHist(hist_bases[c],hist_bases[q],CV_COMP_BHATTACHARYYA);
+                    //if similay
+                    if(similarity>0.8)
+                    {
+                        mearged[q]=true;
+                        if(q!=c)
+                        {
+                            //reduce number of segments
+                            newNumOfSegments--;
+                            for(int i = 0; i < segments.rows; i++ )
+                            {
+                                for(int j = 0; j < segments.cols; j++ )
+                                {
+                                    int index = segments.at<int>(i,j);
+                                    //merge the segment q with c
+                                    if(index==q){
+                                        segments.at<int>(i,j) = c;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    numOfSegments = newNumOfSegments;
+}
+
+Mat createSegmentationDisplay(Mat & segments,int numOfSegments,Mat & image)
+{
+    //create a new image
+    Mat wshed(segments.size(), CV_8UC3);
+    //Create color tab for coloring the segments
+    vector<Vec3b> colorTab;
+  for(int i = 0; i < numOfSegments; i++ )
+  {
+      int b = theRNG().uniform(0, 255);
+      int g = theRNG().uniform(0, 255);
+      int r = theRNG().uniform(0, 255);
+
+      colorTab.push_back(Vec3b((uchar)b, (uchar)g, (uchar)r));
+  }
+
+    //assign different color to different segments
+    for(int i = 0; i < segments.rows; i++ )
+    {
+        for(int j = 0; j < segments.cols; j++ )
+        {
+            int index = segments.at<int>(i,j);
+            if( index == -1 )
+                wshed.at<Vec3b>(i,j) = Vec3b(255,255,255);
+            else if( index <= 0 || index > numOfSegments )
+                wshed.at<Vec3b>(i,j) = Vec3b(0,0,0);
+            //else
+              //  wshed.at<Vec3b>(i,j) = colorTab[index - 1];
+        }
+    }
+
+    //If the original image available then merge with the colors of segments
+    if(image.dims>0) wshed = wshed*0.5+image*0.5;
+
+    return wshed;
+}
+
+Mat watershedSegment(Mat & image, int & noOfSegments)
+{
+    //To store the gray version of the image
+    Mat gray;
+    //To store the thresholded image
+    Mat ret;
+    //convert the image to grayscale
+    cvtColor(image,gray,CV_BGR2GRAY);
+    //imshow("Gray Image",gray);
+    //threshold the image
+    threshold(gray,ret,0,255,CV_THRESH_BINARY_INV+CV_THRESH_OTSU);
+    //imshow("Image after OTSU Thresholding",ret);
+    //Execute morphological-open
+    morphologyEx(ret,ret,MORPH_OPEN,Mat::ones(9,9,CV_8SC1),Point(4,4),2);
+    //imshow("Thresholded Image after Morphological open",ret);
+    //get the distance transformation
+    Mat distTransformed(ret.rows,ret.cols,CV_32FC1);
+    distanceTransform(ret,distTransformed,CV_DIST_L2,3);
+    //normalize the transformed image in order to display
+    normalize(distTransformed, distTransformed, 0.0, 1, NORM_MINMAX);
+    //imshow("Distance Transformation",distTransformed);
+    //threshold the transformed image to obtain markers for watershed
+    threshold(distTransformed,distTransformed,0.1,1,CV_THRESH_BINARY);
+    //Renormalize to 0-255 to further calculations
+    normalize(distTransformed, distTransformed, 0.0, 255.0, NORM_MINMAX);
+    distTransformed.convertTo(distTransformed,CV_8UC1);
+    //imshow("Thresholded Distance Transformation",distTransformed);
+
+    //calculate the contours of markers
+    int i, j, compCount = 0;
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+    findContours(distTransformed, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
+
+    if( contours.empty() )
+        return Mat();
+    Mat markers(distTransformed.size(), CV_32S);
+    markers = Scalar::all(0);
+    int idx = 0;
+    //draw contours
+    for( ; idx >= 0; idx = hierarchy[idx][0], compCount++ )
+        drawContours(markers, contours, idx, Scalar::all(compCount+1), -1, 8, hierarchy, INT_MAX);
+
+    if( compCount == 0 )
+        return Mat();
+
+    //calculate the time taken to the watershed algorithm
+    double t = (double)getTickCount();
+    //apply watershed with the markers as seeds
+    watershed( image, markers );
+    t = (double)getTickCount() - t;
+    printf( "execution time = %gms\n", t*1000./getTickFrequency() );
+
+    //create displayable image of segments
+    Mat wshed = createSegmentationDisplay(markers,compCount,image);
+
+  //imshow( "watershed transform", wshed );
+    noOfSegments = compCount;
+
+    //returns the segments
+    return markers;
+}
+
+Mat multi_channelSegmentation(Mat src){
+
+    int numOfSegments = 0;
+    //Apply watershed
+    Mat segments = watershedSegment(src,numOfSegments);
+    //Merge segments in order to reduce over segmentation
+    mergeSegments(src,segments, numOfSegments);
+    //To display the merged segments
+    Mat wshed = createSegmentationDisplay(segments, numOfSegments, src);
+    //To display the merged segments blended with the image
+    //Mat wshedWithImage = createSegmentationDisplay(segments, numOfSegments, src);
+    return wshed;
+
+}
+
 void ProcessImage(const string image_url){
     using namespace boost::algorithm;
     vector<string> tokens;
@@ -130,10 +303,10 @@ void ProcessImage(const string image_url){
     string output_path = join(tokens, "/");
     cout<<"Output Path: "<<output_path<<endl;
     Mat sourceImage = imread(image_url);
-    //Mat outputImage = multi_channelSegmentation(sourceImage);
-    Mat img_descriptors = SURF_Feature_Detector(sourceImage);
-    namedWindow("surf");
-    //imshow("surf", img_descriptors);
+    Mat outputImage = multi_channelSegmentation(sourceImage);
+    //Mat img_descriptors = SURF_Feature_Detector(sourceImage);
+    namedWindow("seg");
+    imshow("seg", outputImage);
     waitKey(0);
 }
 
